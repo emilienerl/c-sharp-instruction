70510ecb1423e797bae6e947c9d92c50f51847e6
Emilie Nerl <emilie.nerl1@gmail.com>
2020-12-02 20:48:18 -0500

    fixing gitignore

---
+++ ch02-project1-student-registration/ch02-project1-student-registration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ch02_project1_student_registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Registration Form");
+
+            Console.WriteLine("Enter first name: ");
+            String firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name: ");
+            String lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter year of birth: ");
+            String birthYear = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + firstName +" " + lastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + firstName + " " + "* " + birthYear);
+        }
+    }
+}
---
+++ ch04-project1-table-of-powers/ch04-project1-table-of-powers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ch04_project1_table_of_powers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes table");
+
+            String choice = "y";
+            while (choice.Equals("y"))
+            {
+
+                Console.Write("Enter a integer: ");
+                int n = int.Parse(Console.ReadLine());
+
+                
+
+                Console.WriteLine("Number       Square         Cube");
+
+                
+            }
+
+            Console.Write("Continue? ");
+            choice = Console.ReadLine();
+        }
+    }
+}
